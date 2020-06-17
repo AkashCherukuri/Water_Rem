160ff21d5ea0532323b7f92839da41cb2a23fff3
Akash Cherukuri <62202731+LightDrifter@users.noreply.github.com>
2020-06-17 11:41:13 +0530

    Update shows pop-up to save alarm time

--- drink.py
+++ drink.py
@@ -1,5 +1,7 @@
+import pickle
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
 # from kivy.uix.label import Label
 # from kivy.uix.textinput import TextInput
 # from kivy.uix.button import Button
@@ -7,9 +9,9 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.properties import ObjectProperty
 # from kivy.graphics import Rectangle, Color
 from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.popup import Popup
 
-class SetScreen(Screen):
-	pass
+Alarm_time = 0
 
 class Manager(ScreenManager):
 	pass
@@ -25,8 +27,26 @@ class MainScreen(Screen):
 		self.progress.text = str(self.goal.value*100//7000)+'%'
 		print(self.progress.text)
 		print(self.goal.value)
-        
-        
+
+class SetScreen(Screen):
+	#alarm_time = ObjectProperty(None)
+	def alarm_btn(self):
+		return set_alarm_popup()
+
+
+class Alarm_Popup(FloatLayout):
+	alarm_in = ObjectProperty(None)
+
+	def save_alarm(self):
+		Alarm_time = self.alarm_in.text
+		print(f"Saved! Alarm go off in {Alarm_time} intervals!")
+
+def set_alarm_popup():
+	show = Alarm_Popup()
+	popupWindow = Popup(title = "Set Alarm interval", content = show, size_hint = (None, None), size = (600, 400))
+	popupWindow.open()
+
+
 class Drink(App):
     def build(self):
         return
