46cb04e558f7f8d54fd276d184649c5f35255c03
Akash Cherukuri <62202731+LightDrifter@users.noreply.github.com>
2020-06-27 21:29:49 +0530

    Update to play song when time is up

--- Popups/SetAlarm.py
+++ Popups/SetAlarm.py
@@ -4,18 +4,26 @@ parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir) 
 
 from utils import Save, Load
+from timer import Timer
 from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import ObjectProperty
 from kivy.uix.popup import Popup
 
 class Alarm_Popup(FloatLayout):
+	def __init__(self):
+		self.timer = Timer()
+		self.timer.Start_Alarm()
+		super().__init__()
+
 	alarm_in = ObjectProperty(None)
 	Alarm_time = 0
+
 	def save_alarm(self):
 		Alarm_time = self.alarm_in.text
 		Save("Alarm_Time", Alarm_time)
-		save = Load("Alarm_Time")
-		print(f"Saved! Alarm go off in {save} intervals!")
+		self.timer.Cancel_Alarm()
+		self.timer.Start_Alarm()
+		print(f"Saved! Alarm go off in {Alarm_time} intervals!")
 
 def set_alarm_popup():
 	show = Alarm_Popup()
